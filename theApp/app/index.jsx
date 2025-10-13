67eacfb757c62557c965fca088da4561263f0716
albinbujupii <albin.bujupi1@student.uni-pr.edu>
2025-10-14 00:19:33 +0200

    ndryshim

--- theApp/app/index.jsx
+++ theApp/app/index.jsx
@@ -104,7 +104,7 @@ const styles = StyleSheet.create({
     top: 20,
     left: 10,
     paddingVertical: 12,
-    paddingHorizontal: 24,
+    paddingHorizontal: 18,
     borderRadius: 25,
     zIndex: 1000,
   },
