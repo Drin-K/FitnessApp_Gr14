d2877fe1369fc687166be42f3d32e42677e570ee
Drin-K <161349561+Drin-K@users.noreply.github.com>
2025-11-10 16:16:24 +0100

    Seting up the backend

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAWi2ptw59KaW1vF7-oSX8pYX0M50WwGY4",
+  authDomain: "fitnessdb-7e7a8.firebaseapp.com",
+  projectId: "fitnessdb-7e7a8",
+  storageBucket: "fitnessdb-7e7a8.firebasestorage.app",
+  messagingSenderId: "765344318453",
+  appId: "1:765344318453:web:1058134ad5d60266d316bc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
